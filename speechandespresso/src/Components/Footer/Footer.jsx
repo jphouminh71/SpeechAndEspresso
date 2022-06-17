4096c4aa171c7f65602130742794ae50b50a95fb
jphouminh71 <jphouminh71@yahoo.com>
2022-06-17 01:37:34 -0600

    Made footer scale

--- speechandespresso/src/Components/Footer/Footer.jsx
+++ speechandespresso/src/Components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
 import './Footer.css';
 
-/* Going to the same across all components */
 function Footer()
 {
     return (
-        <div className="stick-to-bottom"> 
-            <h1 style={{backgroundColor: "blue"}}> Footer </h1> 
+        <div className="footer-container"> 
+            
+            <div className="contact-information">
+                    <p> Email:  kdang98@gmail.com </p>
+                    <p> Copyright SpeechAndEspresso </p> 
+                </div> 
+                <div className="social-media-links">
+                        <p> About Me</p>
+                        <p> Privacy Policy </p>
+                        <p> Shipping Policy </p>
+                        <p> Refund Policy </p> 
+                        <p> Terms of Service</p>
+                        <p> facebook </p>
+                        <p> Instagram</p>
+                        <p> Leave a review! </p> 
+                    </div>
+                    <div className="news-letter-signup">
+                            <p> Sign up for our Newsletter </p>
+                            <input type="text" />  
+                        </div> 
+                        
         </div>
     );
 }
